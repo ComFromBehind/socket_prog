3b5dbc411575203747e2f8f046749bb2e0705812
yong <lyg3715371537@gmail.com>
2024-06-18 02:06:44 +0900

    impl my_udp_chatting_serv&clnt

---
+++ assignment(self)/udp_chatting_serv.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 30
+void error_handling(char *message);
+
+int main(int argc, char*argv[])
+{
+    int serv_sock;
+    char message2[BUF_SIZE];
+    char message[BUF_SIZE];
+    int str_len;
+    socklen_t clnt_adr_sz;
+
+    struct sockaddr_in serv_adr, clnt_adr;
+    if(argc!=2){
+        printf("Usage : %s <port> \n", argv[0]);
+        exit(1);
+    }
+
+    serv_sock = socket(PF_INET, SOCK_DGRAM, 0);     //UDP 소켓의 생성
+    if(serv_sock==-1)
+        error_handling("UDP socket creation error");
+    
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_adr.sin_port = htons(atoi(argv[1]));
+
+    if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+        error_handling("bind() error");
+    
+    while(1){
+        clnt_adr_sz = sizeof(clnt_adr);
+        str_len = recvfrom(serv_sock, message, BUF_SIZE, 0, 
+            (struct sockaddr*)&clnt_adr, &clnt_adr_sz);     
+        message[str_len] = 0;
+        printf("Message from client : %s\n", message);
+        message[0]= 0;
+
+        printf("client에게 하고 싶은 말: ");
+        fgets(message2, sizeof(message2), stdin);
+        
+        sendto(serv_sock, message2, str_len, 0, 
+            (struct sockaddr*)&clnt_adr, clnt_adr_sz);      //수신된 데이터를 역으로 재전송하기 위해 바로 위 함수에서 얻은 clnt_adr 정보를 이용한다. 
+        message2[0] = 0;
+    }
+    close(serv_sock);
+    return 0;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
