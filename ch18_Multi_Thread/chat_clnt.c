2c9c0e24f4e11030c988ac64def2b3be2d8db3eb
yong <lyg3715371537@gmail.com>
2024-06-29 01:56:50 +0900

    impl multi_thread_server

--- ch18_Multi_Thread/chat_clnt.c
+++ ch18_Multi_Thread/chat_clnt.c
@@ -59,17 +59,20 @@ void * send_msg(void * arg)
         sprintf(name_msg, "%s %s", name, msg);
         write(sock, name_msg, strlen(name_msg));
     }
+    
     return NULL;
 }
 
 void * recv_msg(void*arg)
-{
+{   
+    
     int sock = *((int*)arg);
     char name_msg[NAME_SIZE+BUF_SIZE];
     int str_len;
     while(1)
     {
         str_len = read(sock, name_msg, NAME_SIZE+BUF_SIZE-1);
+        
         if(str_len==-1)
             return (void*)-1;
         name_msg[str_len] = 0;
